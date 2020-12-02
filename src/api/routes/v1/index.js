efe6155230192ac58ff2cafbfcb4a93b329dc009
duybt <duybt@tandan.com.vn>
2020-12-02 15:29:49 +0700

    'update'

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,10 +1,4 @@
 const express = require('express');
-const authRoutes = require('./auth.route');
-const userRoutes = require('./user.route');
-const roleRoutes = require('./role.route');
-const permissionRoutes = require('./permission.route');
-const officeRoutes = require('./office.route');
-const positionRoutes = require('./position.route');
 const subcribeRoutes = require('./subcribe.route');
 const placeRoutes = require('./place.route');
 const scheduleDetailRoutes = require('./scheduleDetail.route');
@@ -28,12 +22,6 @@ router.get('/status', (req, res) => res.send('OK'));
  * GET v1/docs
  */
 router.use('/docs', express.static('docs'));
-router.use('/role', roleRoutes);
-router.use('/permission', permissionRoutes);
-router.use('/office', officeRoutes);
-router.use('/position', positionRoutes);
-router.use('/auth', authRoutes);
-router.use('/user', userRoutes);
 router.use('/subcribe', subcribeRoutes);
 router.use('/place', placeRoutes);
 router.use('/scheduleDetail', scheduleDetailRoutes);
--- src/config/mssql.js
+++ src/config/mssql.js
@@ -19,15 +19,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.users = require('../api/models/user.model')(sequelize, Sequelize);
-db.positions = require('../api/models/position.model')(sequelize, Sequelize);
-db.offices = require('../api/models/office.model')(sequelize, Sequelize);
-db.passwordResetTokens = require('../api/models/passwordResetToken.model')(sequelize, Sequelize);
-db.refreshTokens = require('../api/models/refreshToken.model')(sequelize, Sequelize);
-
-db.roles = require('../api/models/role.model')(sequelize, Sequelize);
-db.permissions = require('../api/models/permission.model')(sequelize, Sequelize);
-db.role_permission = require('../api/models/role_permission.model')(sequelize, Sequelize);
 
 db.attachment = require('../api/models/attachment.model')(sequelize, Sequelize);
 
@@ -41,74 +32,5 @@ db.intro = require('../api/models/intro.model')(sequelize, Sequelize);
 db.bookTour = require('../api/models/bookTour.model')(sequelize, Sequelize);
 db.image = require('../api/models/image.model')(sequelize, Sequelize);
 
-
-const User = db.users;
-const Position = db.positions;
-const Office = db.offices;
-const Role = db.roles;
-const Permission = db.permissions;
-const Attachment = db.attachment;
-const Subcribe = db.subcribe;
-const Place = db.place;
-const ScheduleDetail = db.scheduleDetail;
-const Tour = db.tour;
-const Intro = db.intro;
-const BookTour = db.bookTour;
-const Image = db.image;
-
-
-Position.hasMany(User, { as: 'users' });
-User.belongsTo(Position, {
-  foreignKey: 'positionId',
-  as: 'position',
-});
-
-Office.hasMany(Office, { as: 'offices' });
-Office.belongsTo(Office, {
-  foreignKey: 'parentId',
-  as: 'office',
-});
-
-Office.hasMany(User, { as: 'users' });
-User.belongsTo(Office, {
-  foreignKey: 'officeId',
-  as: 'office',
-});
-
-Office.hasMany(User, { as: 'nhoms' });
-User.belongsTo(Office, {
-  foreignKey: 'nhomId',
-  as: 'nhom',
-});
-
-User.belongsToMany(Role, {
-  through: 'user_role',
-  as: 'roles',
-  foreignKey: 'userId',
-});
-
-Role.belongsToMany(User, {
-  through: 'user_role',
-  as: 'users',
-  foreignKey: 'roleId',
-});
-
-Permission.belongsToMany(Role, {
-  through: 'role_permission',
-  as: 'roles',
-  foreignKey: 'permissionId',
-});
-
-Role.belongsToMany(Permission, {
-  through: 'role_permission',
-  as: 'permissions',
-  foreignKey: 'roleId',
-});
-
-User.hasMany(Attachment, { as: 'attachments' });
-Attachment.belongsTo(User, {
-  foreignKey: 'userId',
-  as: 'user',
-});
 
 module.exports = db;
